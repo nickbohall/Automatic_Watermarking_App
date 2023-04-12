5f6353da4531d754dfe6497d2c65e544b324c2cc
NickBohall <bohall.nick@gmail.com>
2023-04-12 15:08:33 -0500

    Added global variables for easy text, color, rotation changing

--- watermark.py
+++ watermark.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageDraw, ImageFont
 from tkinter import filedialog
 from tkinter import Tk
 
+WATERMARK_TEXT = "Yoonicaps"
+WATERMARK_ROTATION = 30
+WATERMARK_COLOR = (204, 204, 255, 50)
+
 # Create tkinter window
 root = Tk()
 root.withdraw()
@@ -29,7 +33,7 @@ def add_watermark(image, wm_text):
     x, y = int(image_width / 2), int(image_height / 2)
 
     # Add the watermark to the blank RGBA image
-    draw.text((x, y), wm_text, font=font, fill=(204, 204, 255, 50), stroke_width=3, stroke_fill=(34,2,0,125), anchor='ms')
+    draw.text((x, y), wm_text, font=font, fill=WATERMARK_COLOR, stroke_width=3, stroke_fill=(34,2,0,125), anchor='ms')
 
     # Combine the watermark RGBA image with the original image to get transparency effect
     rgba_im = rgba_im.rotate(30)
@@ -40,5 +44,5 @@ def add_watermark(image, wm_text):
     watermarked.save(f"watermarked_images/marked_{outputname}")
 
 
-add_watermark(filename, "Yoonicaps")
+add_watermark(filename, WATERMARK_TEXT)
 
